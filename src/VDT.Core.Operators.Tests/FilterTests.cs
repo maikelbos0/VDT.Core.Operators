00b9ded1fd7916395c9077f2ac8398b5c8a8ec8a
Maikel Bos <maikel.bos0@gmail.com>
2024-02-01 19:59:42 +0100

    Add filter operator

---
+++ src/VDT.Core.Operators.Tests/FilterTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace VDT.Core.Operators.Tests;
+
+public class FilterTests {
+    [Fact]
+    public async Task ReturnsDismissedWhenNotMatched() {
+        var subject = new Filter<string>(value => value.StartsWith('B'));
+
+        var result = await subject.Execute("Foo");
+
+        Assert.False(result.IsAccepted);
+    }
+
+    [Fact]
+    public async Task ReturnsAcceptedWhenMatched() {
+        var subject = new Filter<string>(value => value.StartsWith('B'));
+
+        var result = await subject.Execute("Bar");
+
+        Assert.True(result.IsAccepted);
+    }
+}
---
+++ src/VDT.Core.Operators/Filter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace VDT.Core.Operators;
+
+public class Filter<TValue> : IOperator<TValue, TValue> {
+    private readonly Func<TValue, Task<bool>> predicate;
+
+    public Filter(Func<TValue, bool> predicate)
+        : this(value => Task.FromResult(predicate(value))) { }
+
+    public Filter(Func<TValue, Task<bool>> predicate) {
+        this.predicate = predicate;
+    }
+
+    public async Task<OperationResult<TValue>> Execute(TValue value) {
+        if (await predicate(value)) {
+            return OperationResult<TValue>.Accepted(value);
+        }
+        else {
+            return OperationResult<TValue>.Dismissed();
+        }
+    }
+}
