9a26b497ca7c2e14cde49d7e32eb62f27dcf9653
Maikel Bos <maikel.bos0@gmail.com>
2024-02-05 18:01:55 +0100

    Remove value and add CancellationToken parameter to delay function and bring all constructor arguments under test

--- src/VDT.Core.Operators.Tests/DebounceTests.cs
+++ src/VDT.Core.Operators.Tests/DebounceTests.cs
@@ -8,7 +8,7 @@ namespace VDT.Core.Operators.Tests;
 
 public class DebounceTests {
     [Fact]
-    public async Task DelaysForInterval() {
+    public async Task DelaysForInterval_Constant() {
         var subject = new Debounce<string>(500);
         var targetStream = Substitute.For<IOperandStream<string>>();
         var cancellationTokenSource = new CancellationTokenSource();
@@ -17,6 +17,49 @@ public class DebounceTests {
 
         await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
 
+        await subject.Delay.Received().Invoke(500, cancellationTokenSource.Token);
+    }
+
+    [Fact]
+    public async Task DelaysForInterval_Function() {
+        var subject = new Debounce<string>(() => 500);
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        subject.Delay = Substitute.For<Func<int, CancellationToken, Task>>();
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await subject.Delay.Received().Invoke(500, cancellationTokenSource.Token);
+    }
+
+    [Fact]
+    public async Task DelaysForInterval_TaskFunction() {
+        var subject = new Debounce<string>(() => Task.FromResult(500));
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        subject.Delay = Substitute.For<Func<int, CancellationToken, Task>>();
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await subject.Delay.Received().Invoke(500, cancellationTokenSource.Token);
+    }
+
+    [Fact]
+    public async Task DelaysForInterval_CancellableTaskFunction() {
+        var func = Substitute.For<Func<CancellationToken, Task<int>>>();
+        var subject = new Debounce<string>(func);
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        func.Invoke(Arg.Any<CancellationToken>()).Returns(500);
+
+        subject.Delay = Substitute.For<Func<int, CancellationToken, Task>>();
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await func.Received().Invoke(cancellationTokenSource.Token);
         await subject.Delay.Received().Invoke(500, cancellationTokenSource.Token);
     }
 
--- src/VDT.Core.Operators/Debounce.cs
+++ src/VDT.Core.Operators/Debounce.cs
@@ -5,18 +5,21 @@ using System.Threading.Tasks;
 namespace VDT.Core.Operators;
 
 public class Debounce<TValue> : IOperator<TValue, TValue> {
-    private readonly Func<TValue, Task<int>> delayFunc;
+    private readonly Func<CancellationToken, Task<int>> delayFunc;
     private int operationId = 0;
 
     internal Func<int, CancellationToken, Task> Delay { get; set; } = Task.Delay;
 
     public Debounce(int delayInMilliseconds)
-        : this(value => Task.FromResult(delayInMilliseconds)) { }
+        : this(_ => Task.FromResult(delayInMilliseconds)) { }
 
-    public Debounce(Func<TValue, int> delayFunc)
-        : this(value => Task.FromResult(delayFunc(value))) { }
+    public Debounce(Func<int> delayFunc)
+        : this(_ => Task.FromResult(delayFunc())) { }
 
-    public Debounce(Func<TValue, Task<int>> delayFunc) {
+    public Debounce(Func<Task<int>> delayFunc)
+        : this(_ => delayFunc()) { }
+
+    public Debounce(Func<CancellationToken, Task<int>> delayFunc) {
         this.delayFunc = delayFunc;
     }
 
@@ -24,7 +27,7 @@ public class Debounce<TValue> : IOperator<TValue, TValue> {
         // Since Interlocked.Increment wraps, this will debounce properly until 2^32 operations occur in the delay
         var expectedOperationId = Interlocked.Increment(ref operationId);
 
-        await Delay(await delayFunc(value), cancellationToken);
+        await Delay(await delayFunc(cancellationToken), cancellationToken);
 
         if (operationId == expectedOperationId) {
             await targetStream.Write(value, cancellationToken);
