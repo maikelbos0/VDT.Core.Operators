9061b2c28ceeabdf74f90a3cc20b07b34cecb699
Maikel Bos <maikel.bos0@gmail.com>
2024-02-05 17:33:42 +0100

    Add CancellationToken parameter to predicate and bring all constructor arguments under test

--- src/VDT.Core.Operators.Tests/FilterTests.cs
+++ src/VDT.Core.Operators.Tests/FilterTests.cs
@@ -1,4 +1,5 @@
 ﻿using NSubstitute;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -7,7 +8,7 @@ namespace VDT.Core.Operators.Tests;
 
 public class FilterTests {
     [Fact]
-    public async Task DoesNotWriteWhenNotMatched() {
+    public async Task DoesNotWriteWhenNotMatched_Predicate() {
         var subject = new Filter<string>(value => value.StartsWith('B'));
         var targetStream = Substitute.For<IOperandStream<string>>();
         var cancellationTokenSource = new CancellationTokenSource();
@@ -18,13 +19,65 @@ public class FilterTests {
     }
 
     [Fact]
-    public async Task WritesWhenMatched() {
+    public async Task WritesWhenMatched_Predicate() {
         var subject = new Filter<string>(value => value.StartsWith('B'));
         var targetStream = Substitute.For<IOperandStream<string>>();
         var cancellationTokenSource = new CancellationTokenSource();
 
         await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
 
+        await targetStream.Received().Write("Bar", cancellationTokenSource.Token);
+    }
+
+    [Fact]
+    public async Task DoesNotWriteWhenNotMatched_PredicateTask() {
+        var subject = new Filter<string>(value => Task.FromResult(value.StartsWith('B')));
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await targetStream.DidNotReceive().Write(Arg.Any<string>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task WritesWhenMatched_PredicateTask() {
+        var subject = new Filter<string>(value => Task.FromResult(value.StartsWith('B')));
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+
+        await targetStream.Received().Write("Bar", cancellationTokenSource.Token);
+    }
+
+    [Fact]
+    public async Task DoesNotWriteWhenNotMatched_CancellablePredicateTask() {
+        var predicate = Substitute.For<Func<string, CancellationToken, Task<bool>>>();
+        var subject = new Filter<string>(predicate);
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        predicate.Invoke(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(callInfo => callInfo.ArgAt<string>(0).StartsWith('B'));
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await predicate.Received().Invoke("Foo", cancellationTokenSource.Token);
+        await targetStream.DidNotReceive().Write(Arg.Any<string>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task WritesWhenMatched_CancellablePredicateTask() {
+        var predicate = Substitute.For<Func<string, CancellationToken, Task<bool>>>();
+        var subject = new Filter<string>(predicate);
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        predicate.Invoke(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(callInfo => callInfo.ArgAt<string>(0).StartsWith('B'));
+
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+
+        await predicate.Received().Invoke("Bar", cancellationTokenSource.Token);
         await targetStream.Received().Write("Bar", cancellationTokenSource.Token);
     }
 }
--- src/VDT.Core.Operators/Filter.cs
+++ src/VDT.Core.Operators/Filter.cs
@@ -5,17 +5,20 @@ using System.Threading.Tasks;
 namespace VDT.Core.Operators;
 
 public class Filter<TValue> : IOperator<TValue, TValue> {
-    private readonly Func<TValue, Task<bool>> predicate;
+    private readonly Func<TValue, CancellationToken, Task<bool>> predicate;
 
     public Filter(Func<TValue, bool> predicate)
-        : this(value => Task.FromResult(predicate(value))) { }
+        : this((value, _) => Task.FromResult(predicate(value))) { }
 
-    public Filter(Func<TValue, Task<bool>> predicate) {
+    public Filter(Func<TValue, Task<bool>> predicate)
+        : this((value, _) => predicate(value)) { }
+
+    public Filter(Func<TValue, CancellationToken, Task<bool>> predicate) {
         this.predicate = predicate;
     }
 
     public async Task Execute(TValue value, IOperandStream<TValue> targetStream, CancellationToken cancellationToken) {
-        if (await predicate(value)) {
+        if (await predicate(value, cancellationToken)) {
             await targetStream.Write(value, cancellationToken);
         }
     }
