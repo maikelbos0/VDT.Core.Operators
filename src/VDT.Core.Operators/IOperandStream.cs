90aae86786c95abdf1f106c373cce941b15c01f4
Maikel Bos <maikel.bos0@gmail.com>
2024-02-06 19:31:15 +0100

    Add first bit of xml documentation

--- src/VDT.Core.Operators/IOperandStream.cs
+++ src/VDT.Core.Operators/IOperandStream.cs
@@ -4,11 +4,49 @@ using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
+/// <summary>
+/// Represents a stream of values that can be subscribed to and transformed
+/// </summary>
+/// <typeparam name="TValue">Type of the values</typeparam>
 public interface IOperandStream<TValue> {
+    /// <summary>
+    /// Write a value to this stream
+    /// </summary>
+    /// <param name="value">The value</param>
+    /// <returns>A <see cref="Task"/> which completes asynchronously once the value has been consumed by all subscribers and operators</returns>
     Task Write(TValue value);
+
+    /// <summary>
+    /// Write a value to this stream
+    /// </summary>
+    /// <param name="value">The value</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete</param>
+    /// <returns>A <see cref="Task"/> which completes asynchronously once the value has been consumed by all subscribers and operators</returns>
     Task Write(TValue value, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Subscribe a method to receive values written to this stream
+    /// </summary>
+    /// <param name="subscriber">Method that handles the received value</param>
     void Subscribe(Action<TValue> subscriber);
+
+    /// <summary>
+    /// Subscribe a method to receive values written to this stream
+    /// </summary>
+    /// <param name="subscriber">Method that handles the received value</param>
     void Subscribe(Func<TValue, Task> subscriber);
+
+    /// <summary>
+    /// Subscribe a method to receive values written to this stream
+    /// </summary>
+    /// <param name="subscriber">Method that handles the received value</param>
     void Subscribe(Func<TValue, CancellationToken, Task> subscriber);
+
+    /// <summary>
+    /// Pass values written to this stream to the supplied operator for transformation and write them to a target stream
+    /// </summary>
+    /// <typeparam name="TTransformedValue">Type of the value after transformation</typeparam>
+    /// <param name="op">Operator that will write values to the target stream</param>
+    /// <returns>The target stream the supplied operator will write transformed values to</returns>
     IOperandStream<TTransformedValue> Pipe<TTransformedValue>(IOperator<TValue, TTransformedValue> op);
 }
--- src/VDT.Core.Operators/OperandStream.cs
+++ src/VDT.Core.Operators/OperandStream.cs
@@ -6,27 +6,34 @@ using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
+/// <inheritdoc/>
 public class OperandStream<TValue> : IOperandStream<TValue> {
     private readonly List<Func<TValue, CancellationToken, Task>> subscribers = [];
 
+    /// <inheritdoc/>
     public Task Write(TValue value)
         => Write(value, CancellationToken.None);
 
+    /// <inheritdoc/>
     public Task Write(TValue value, CancellationToken cancellationToken)
         => Task.WhenAll(subscribers.Select(subscriber => subscriber(value, cancellationToken)));
 
+    /// <inheritdoc/>
     public void Subscribe(Action<TValue> subscriber)
         => subscribers.Add((value, _) => {
             subscriber(value);
             return Task.CompletedTask;
         });
 
+    /// <inheritdoc/>
     public void Subscribe(Func<TValue, Task> subscriber)
         => subscribers.Add((value, _) => subscriber(value));
 
+    /// <inheritdoc/>
     public void Subscribe(Func<TValue, CancellationToken, Task> subscriber)
         => subscribers.Add(subscriber);
 
+    /// <inheritdoc/>
     public IOperandStream<TTransformedValue> Pipe<TTransformedValue>(IOperator<TValue, TTransformedValue> op) {
         var targetStream = new OperandStream<TTransformedValue>();
 
