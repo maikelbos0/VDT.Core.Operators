adfd47fc60dafce926ba6aa45ac64b3837788387
Maikel Bos <maikel.bos0@gmail.com>
2024-02-13 20:04:49 +0100

    Add source generator for creating extension methods

--- src/VDT.Core.Operators/IOperator.cs
+++ src/VDT.Core.Operators/IOperator.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
+// TODO add initializing method and options somehow, so we can make Merge an operator
 public interface IOperator<TValue, TTransformedValue> {
     Task Execute(TValue value, IOperandStream<TTransformedValue> targetStream, CancellationToken cancellationToken);
 }
