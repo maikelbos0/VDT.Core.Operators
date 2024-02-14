d70ee0158908fe944f9e0ee3e82a21891af137d1
Maikel Bos <maikel.bos0@gmail.com>
2024-02-14 19:21:15 +0100

    Add debounce extension overloads

--- src/VDT.Core.Operators/OperandStreamDebounceExtensions.cs
+++ src/VDT.Core.Operators/OperandStreamDebounceExtensions.cs
@@ -1,7 +1,19 @@
-﻿namespace VDT.Core.Operators;
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System;
 
-// TODO add overloads
+namespace VDT.Core.Operators;
+
 public static class OperandStreamDebounceExtensions {
     public static IOperandStream<TValue> Debounce<TValue>(this IOperandStream<TValue> operandStream, int delayInMilliseconds)
         => operandStream.Pipe(new Debounce<TValue>(delayInMilliseconds));
+
+    public static IOperandStream<TValue> Debounce<TValue>(this IOperandStream<TValue> operandStream, Func<int> delayFunc)
+        => operandStream.Pipe(new Debounce<TValue>(delayFunc));
+
+    public static IOperandStream<TValue> Debounce<TValue>(this IOperandStream<TValue> operandStream, Func<Task<int>> delayFunc)
+        => operandStream.Pipe(new Debounce<TValue>(delayFunc));
+
+    public static IOperandStream<TValue> Debounce<TValue>(this IOperandStream<TValue> operandStream, Func<CancellationToken, Task<int>> delayFunc)
+        => operandStream.Pipe(new Debounce<TValue>(delayFunc));
 }
