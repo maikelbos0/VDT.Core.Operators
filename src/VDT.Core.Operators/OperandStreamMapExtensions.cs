b55b57b4861b18354dcc73907604341bef244774
Maikel Bos <maikel.bos0@gmail.com>
2024-02-14 19:26:30 +0100

    Add map extension overloads and fix map test names

--- src/VDT.Core.Operators/OperandStreamMapExtensions.cs
+++ src/VDT.Core.Operators/OperandStreamMapExtensions.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
 // TODO add overloads
 public static class OperandStreamMapExtensions {
     public static IOperandStream<TTransformedValue> Map<TValue, TTransformedValue>(this IOperandStream<TValue> operandStream, Func<TValue, TTransformedValue> func)
+        => operandStream.Pipe(new Map<TValue, TTransformedValue>(func));
+
+    public static IOperandStream<TTransformedValue> Map<TValue, TTransformedValue>(this IOperandStream<TValue> operandStream, Func<TValue, Task<TTransformedValue>> func)
+        => operandStream.Pipe(new Map<TValue, TTransformedValue>(func));
+
+    public static IOperandStream<TTransformedValue> Map<TValue, TTransformedValue>(this IOperandStream<TValue> operandStream, Func<TValue, CancellationToken, Task<TTransformedValue>> func)
         => operandStream.Pipe(new Map<TValue, TTransformedValue>(func));
 }
