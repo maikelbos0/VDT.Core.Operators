6c89acb673d8282c25930800cac341620008f38c
Maikel Bos <maikel.bos0@gmail.com>
2024-02-19 16:44:17 +0100

    Add documentation to debounce

--- src/VDT.Core.Operators/Debounce.cs
+++ src/VDT.Core.Operators/Debounce.cs
@@ -4,25 +4,46 @@ using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
+/// <summary>
+/// Operator that publishes only the last value received in succession after a specified delay, discarding those values received while the delay had not finished
+/// </summary>
+/// <typeparam name="TValue">Type of value to debounce</typeparam>
 public class Debounce<TValue> : IOperator<TValue, TValue> {
     private readonly Func<CancellationToken, Task<int>> delayFunc;
     private int operationId = 0;
 
     internal Func<int, CancellationToken, Task> Delay { get; set; } = Task.Delay;
 
+    /// <summary>
+    /// Create a debounce operator
+    /// </summary>
+    /// <param name="delayInMilliseconds">Time in milliseconds to wait before publishing a value</param>
     public Debounce(int delayInMilliseconds)
         : this(_ => Task.FromResult(delayInMilliseconds)) { }
 
+    /// <summary>
+    /// Create a debounce operator
+    /// </summary>
+    /// <param name="delayFunc">Method that provides the time in milliseconds to wait before publishing a value</param>
     public Debounce(Func<int> delayFunc)
         : this(_ => Task.FromResult(delayFunc())) { }
 
+    /// <summary>
+    /// Create a debounce operator
+    /// </summary>
+    /// <param name="delayFunc">Method that provides the time in milliseconds to wait before publishing a value</param>
     public Debounce(Func<Task<int>> delayFunc)
         : this(_ => delayFunc()) { }
 
+    /// <summary>
+    /// Create a debounce operator
+    /// </summary>
+    /// <param name="delayFunc">Method that provides the time in milliseconds to wait before publishing a value</param>
     public Debounce(Func<CancellationToken, Task<int>> delayFunc) {
         this.delayFunc = delayFunc;
     }
 
+    /// <inheritdoc/>
     public async Task Execute(TValue value, IOperandStream<TValue> targetStream, CancellationToken cancellationToken) {
         // Since Interlocked.Increment wraps, this will debounce properly until 2^32 operations occur in the delay
         var expectedOperationId = Interlocked.Increment(ref operationId);
