72883786611344021465b06cae1a0febe4b2ce32
Maikel Bos <maikel.bos0@gmail.com>
2024-02-19 16:55:14 +0100

    Add flatten docs

--- src/VDT.Core.Operators/Flatten.cs
+++ src/VDT.Core.Operators/Flatten.cs
@@ -4,7 +4,12 @@ using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
+/// <summary>
+/// Operation that subscribes to an <see cref="IOperandStream{TValue}"/> of type <see cref="OperandStream{TValue}"/> and publishes values of <typeparamref name="TValue"/>
+/// </summary>
+/// <typeparam name="TValue">Type of operand stream to flatten</typeparam>
 public class Flatten<TValue> : IOperator<IOperandStream<TValue>, TValue> {
+    /// <inheritdoc/>
     public Task Execute(IOperandStream<TValue> value, IOperandStream<TValue> targetStream, CancellationToken _) {
         value.Subscribe((Func<TValue, CancellationToken, Task>)targetStream.Publish);
 
