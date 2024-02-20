0d811b5705f81618c68042750b8df82143b3981d
Maikel Bos <maikel.bos0@gmail.com>
2024-02-20 19:45:10 +0100

    Add operand stream group extension methods

---
+++ src/VDT.Core.Operators.Tests/OperandStreamGroupExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using NSubstitute;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VDT.Core.Operators.Tests;
+
+public class OperandStreamGroupExtensionsTests {
+    [Fact]
+    public void Group_Predicate() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.Group(value => value.FirstOrDefault());
+
+        subject.Received().Pipe(Arg.Any<Group<string, char>>());
+    }
+
+    [Fact]
+    public void Group_TaskPredicate() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.Group(value => Task.FromResult(value.FirstOrDefault()));
+
+        subject.Received().Pipe(Arg.Any<Group<string, char>>());
+    }
+
+    [Fact]
+    public void Group_CancellableTaskPredicate() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.Group((value, _) => Task.FromResult(value.FirstOrDefault()));
+
+        subject.Received().Pipe(Arg.Any<Group<string, char>>());
+    }
+}
---
+++ src/VDT.Core.Operators/OperandStreamGroupExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VDT.Core.Operators;
+
+public static class OperandStreamGroupExtensions {
+    public static IOperandStream<List<TValue>> Group<TValue, TKey>(this IOperandStream<TValue> operandStream, Func<TValue, TKey> keySelector)
+        => operandStream.Pipe(new Group<TValue, TKey>(keySelector));
+
+    public static IOperandStream<List<TValue>> Group<TValue, TKey>(this IOperandStream<TValue> operandStream, Func<TValue, Task<TKey>> keySelector)
+        => operandStream.Pipe(new Group<TValue, TKey>(keySelector));
+
+    public static IOperandStream<List<TValue>> Group<TValue, TKey>(this IOperandStream<TValue> operandStream, Func<TValue, CancellationToken, Task<TKey>> keySelector)
+        => operandStream.Pipe(new Group<TValue, TKey>(keySelector));
+}
