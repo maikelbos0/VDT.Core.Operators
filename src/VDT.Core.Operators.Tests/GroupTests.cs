67181d82a26fdc902eeb0a29b943d76f82f7f03e
Maikel Bos <maikel.bos0@gmail.com>
2024-02-20 19:39:27 +0100

    Add group operator

---
+++ src/VDT.Core.Operators.Tests/GroupTests.cs
@@ -0,0 +1,60 @@
+﻿using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VDT.Core.Operators.Tests;
+
+public class GroupTests {
+    [Fact]
+    public async Task GroupsByKeySelector_Function() {
+        var subject = new Group<string, char>(value => value.FirstOrDefault());
+        var targetStream = Substitute.For<IOperandStream<List<string>>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+        await subject.Execute("Baz", targetStream, cancellationTokenSource.Token);
+        await subject.Execute("Qux", targetStream, cancellationTokenSource.Token);
+
+        await targetStream.Received().Publish(Arg.Is<List<string>>(x => x.SequenceEqual(new[] { "Foo" })), cancellationTokenSource.Token);
+        await targetStream.Received().Publish(Arg.Is<List<string>>(x => x.SequenceEqual(new[] { "Bar", "Baz" })), cancellationTokenSource.Token);
+    }
+
+    [Fact]
+    public async Task GroupsByKeySelector_TaskFunction() {
+        var subject = new Group<string, char>(value => Task.FromResult(value.FirstOrDefault()));
+        var targetStream = Substitute.For<IOperandStream<List<string>>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+        await subject.Execute("Baz", targetStream, cancellationTokenSource.Token);
+        await subject.Execute("Qux", targetStream, cancellationTokenSource.Token);
+
+        await targetStream.Received().Publish(Arg.Is<List<string>>(x => x.SequenceEqual(new[] { "Foo" })), cancellationTokenSource.Token);
+        await targetStream.Received().Publish(Arg.Is<List<string>>(x => x.SequenceEqual(new[] { "Bar", "Baz" })), cancellationTokenSource.Token);
+    }
+
+    [Fact]
+    public async Task GroupsByKeySelector_CancellableTaskFunction() {
+        var func = Substitute.For<Func<string, CancellationToken, Task<char>>>();
+        var subject = new Group<string, char>(func);
+        var targetStream = Substitute.For<IOperandStream<List<string>>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        func.Invoke(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(x => x.ArgAt<string>(0).FirstOrDefault());
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+        await subject.Execute("Baz", targetStream, cancellationTokenSource.Token);
+        await subject.Execute("Qux", targetStream, cancellationTokenSource.Token);
+
+        await func.Received().Invoke(Arg.Any<string>(), cancellationTokenSource.Token);
+        await targetStream.Received().Publish(Arg.Is<List<string>>(x => x.SequenceEqual(new[] { "Foo" })), cancellationTokenSource.Token);
+        await targetStream.Received().Publish(Arg.Is<List<string>>(x => x.SequenceEqual(new[] { "Bar", "Baz" })), cancellationTokenSource.Token);
+    }
+}
---
+++ src/VDT.Core.Operators/Group.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VDT.Core.Operators;
+
+public class Group<TValue, TKey> : IOperator<TValue, List<TValue>> {
+    private readonly Func<TValue, CancellationToken, Task<TKey>> keySelector;
+
+    private readonly object groupLock = new();
+    private TKey previousKey;
+    private List<TValue> previousValues = new();
+
+    public Group(Func<TValue, TKey> keySelector)
+        : this((value, _) => Task.FromResult(keySelector(value))) { }
+
+    public Group(Func<TValue, Task<TKey>> keySelector)
+        : this((value, _) => keySelector(value)) { }
+
+    public Group(Func<TValue, CancellationToken, Task<TKey>> keySelector) {
+        this.keySelector = keySelector;
+    }
+
+    public async Task Execute(TValue value, IOperandStream<List<TValue>> targetStream, CancellationToken cancellationToken) {
+        var key = await keySelector(value, cancellationToken);
+        List<TValue>? valuesToPublish = null;
+
+        lock (groupLock) {
+            // TODO use supplied IEqualityComparer<TKey> ?? EqualityComparer<TKey>.Default
+            if (Equals(previousKey, key)) {
+                previousValues.Add(value);
+                return;
+            }
+            else {
+                valuesToPublish = previousValues;
+                previousKey = key;
+                previousValues = new() {
+                    value
+                };
+            }
+        }
+
+        if (valuesToPublish.Count > 0) {
+            await targetStream.Publish(valuesToPublish, cancellationToken);
+        }
+    }
+}
