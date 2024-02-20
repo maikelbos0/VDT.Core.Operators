207f473ba3e4dd8c73183b1df61030aeb4b52be8
Maikel Bos <maikel.bos0@gmail.com>
2024-02-20 20:53:29 +0100

    Add key comparer overloads to group extensions

--- src/VDT.Core.Operators.Tests/OperandStreamGroupExtensionsTests.cs
+++ src/VDT.Core.Operators.Tests/OperandStreamGroupExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using NSubstitute;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -16,6 +17,15 @@ public class OperandStreamGroupExtensionsTests {
     }
 
     [Fact]
+    public void Group_Predicate_EqualityComparer() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.Group(value => value.FirstOrDefault(), EqualityComparer<char>.Default);
+
+        subject.Received().Pipe(Arg.Any<Group<string, char>>());
+    }
+
+    [Fact]
     public void Group_TaskPredicate() {
         var subject = Substitute.For<IOperandStream<string>>();
 
@@ -25,10 +35,28 @@ public class OperandStreamGroupExtensionsTests {
     }
 
     [Fact]
+    public void Group_TaskPredicate_EqualityComparer() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.Group(value => Task.FromResult(value.FirstOrDefault()), EqualityComparer<char>.Default);
+
+        subject.Received().Pipe(Arg.Any<Group<string, char>>());
+    }
+
+    [Fact]
     public void Group_CancellableTaskPredicate() {
         var subject = Substitute.For<IOperandStream<string>>();
 
         _ = subject.Group((value, _) => Task.FromResult(value.FirstOrDefault()));
+
+        subject.Received().Pipe(Arg.Any<Group<string, char>>());
+    }
+
+    [Fact]
+    public void Group_CancellableTaskPredicate_EqualityComparer() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.Group((value, _) => Task.FromResult(value.FirstOrDefault()), EqualityComparer<char>.Default);
 
         subject.Received().Pipe(Arg.Any<Group<string, char>>());
     }
--- src/VDT.Core.Operators/OperandStreamGroupExtensions.cs
+++ src/VDT.Core.Operators/OperandStreamGroupExtensions.cs
@@ -9,9 +9,18 @@ public static class OperandStreamGroupExtensions {
     public static IOperandStream<List<TValue>> Group<TValue, TKey>(this IOperandStream<TValue> operandStream, Func<TValue, TKey> keySelector)
         => operandStream.Pipe(new Group<TValue, TKey>(keySelector));
 
+    public static IOperandStream<List<TValue>> Group<TValue, TKey>(this IOperandStream<TValue> operandStream, Func<TValue, TKey> keySelector, IEqualityComparer<TKey> keyComparer)
+        => operandStream.Pipe(new Group<TValue, TKey>(keySelector, keyComparer));
+
     public static IOperandStream<List<TValue>> Group<TValue, TKey>(this IOperandStream<TValue> operandStream, Func<TValue, Task<TKey>> keySelector)
         => operandStream.Pipe(new Group<TValue, TKey>(keySelector));
 
+    public static IOperandStream<List<TValue>> Group<TValue, TKey>(this IOperandStream<TValue> operandStream, Func<TValue, Task<TKey>> keySelector, IEqualityComparer<TKey> keyComparer)
+        => operandStream.Pipe(new Group<TValue, TKey>(keySelector, keyComparer));
+
     public static IOperandStream<List<TValue>> Group<TValue, TKey>(this IOperandStream<TValue> operandStream, Func<TValue, CancellationToken, Task<TKey>> keySelector)
         => operandStream.Pipe(new Group<TValue, TKey>(keySelector));
+
+    public static IOperandStream<List<TValue>> Group<TValue, TKey>(this IOperandStream<TValue> operandStream, Func<TValue, CancellationToken, Task<TKey>> keySelector, IEqualityComparer<TKey> keyComparer)
+        => operandStream.Pipe(new Group<TValue, TKey>(keySelector, keyComparer));
 }
