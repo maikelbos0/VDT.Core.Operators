8534233f60ddef69b69c2be27142b587bc2fbf3d
Maikel Bos <maikel.bos0@gmail.com>
2024-02-24 07:59:02 +0100

    Add queue throttle operator

---
+++ src/VDT.Core.Operators.Tests/QueueThrottleTests.cs
@@ -0,0 +1,135 @@
+﻿using NSubstitute;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VDT.Core.Operators.Tests;
+
+public class QueueThrottleTests {
+    [Fact]
+    public async Task PublishesFirstValueImmediately() {
+        var subject = new QueueThrottle<string>(500);
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        subject.Delay = Substitute.For<Func<int, CancellationToken, Task>>();
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await targetStream.Received().Publish("Foo", cancellationTokenSource.Token);
+        await subject.Delay.DidNotReceive().Invoke(Arg.Any<int>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task PublishesSecondValueAfterDelay_Constant() {
+        var subject = new QueueThrottle<string>(500);
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        subject.Delay = Substitute.For<Func<int, CancellationToken, Task>>();
+        subject.UtcNow = () => new DateTime(2024, 1, 2, 3, 14, 15, 30, DateTimeKind.Utc);
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+
+        Received.InOrder(() => {
+            targetStream.Publish("Foo", cancellationTokenSource.Token);
+            subject.Delay.Invoke(500, cancellationTokenSource.Token);
+            targetStream.Publish("Bar", cancellationTokenSource.Token);
+        });
+    }
+
+    [Fact]
+    public async Task PublishesSecondValueAfterDelay_Function() {
+        var subject = new QueueThrottle<string>(() => 500);
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        subject.Delay = Substitute.For<Func<int, CancellationToken, Task>>();
+        subject.UtcNow = () => new DateTime(2024, 1, 2, 3, 14, 15, 30, DateTimeKind.Utc);
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+
+        Received.InOrder(() => {
+            targetStream.Publish("Foo", cancellationTokenSource.Token);
+            subject.Delay.Invoke(500, cancellationTokenSource.Token);
+            targetStream.Publish("Bar", cancellationTokenSource.Token);
+        });
+    }
+
+    [Fact]
+    public async Task PublishesSecondValueAfterDelay_TaskFunction() {
+        var subject = new QueueThrottle<string>(() => Task.FromResult(500));
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        subject.Delay = Substitute.For<Func<int, CancellationToken, Task>>();
+        subject.UtcNow = () => new DateTime(2024, 1, 2, 3, 14, 15, 30, DateTimeKind.Utc);
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+
+        Received.InOrder(() => {
+            targetStream.Publish("Foo", cancellationTokenSource.Token);
+            subject.Delay.Invoke(500, cancellationTokenSource.Token);
+            targetStream.Publish("Bar", cancellationTokenSource.Token);
+        });
+    }
+
+    [Fact]
+    public async Task PublishesSecondValueAfterDelay_CancellableTaskFunction() {
+        var func = Substitute.For<Func<CancellationToken, Task<int>>>();
+        var subject = new QueueThrottle<string>(func);
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        func.Invoke(Arg.Any<CancellationToken>()).Returns(500);
+        subject.Delay = Substitute.For<Func<int, CancellationToken, Task>>();
+        subject.UtcNow = () => new DateTime(2024, 1, 2, 3, 14, 15, 30, DateTimeKind.Utc);
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+
+        Received.InOrder(() => {
+            func.Invoke(cancellationTokenSource.Token);
+            targetStream.Publish("Foo", cancellationTokenSource.Token);
+            func.Invoke(cancellationTokenSource.Token);
+            subject.Delay.Invoke(500, cancellationTokenSource.Token);
+            targetStream.Publish("Bar", cancellationTokenSource.Token);
+        });
+    }
+
+    [Fact]
+    public async Task PublishesMoreValuesAfterMultipleDelays() {
+        var subject = new QueueThrottle<string>(500);
+        var targetStream = Substitute.For<IOperandStream<string>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        subject.Delay = Substitute.For<Func<int, CancellationToken, Task>>();
+        subject.UtcNow = () => new DateTime(2024, 1, 2, 3, 14, 15, 30, DateTimeKind.Utc);
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        await subject.Execute("Bar", targetStream, cancellationTokenSource.Token);
+
+        await subject.Execute("Baz", targetStream, cancellationTokenSource.Token);
+
+        await subject.Execute("Qux", targetStream, cancellationTokenSource.Token);
+
+        Received.InOrder(() => {
+            targetStream.Publish("Foo", cancellationTokenSource.Token);
+            subject.Delay.Invoke(500, cancellationTokenSource.Token);
+            targetStream.Publish("Bar", cancellationTokenSource.Token);
+            subject.Delay.Invoke(1000, cancellationTokenSource.Token);
+            targetStream.Publish("Baz", cancellationTokenSource.Token);
+            subject.Delay.Invoke(1500, cancellationTokenSource.Token);
+            targetStream.Publish("Qux", cancellationTokenSource.Token);
+        });
+    }
+}
---
+++ src/VDT.Core.Operators/QueueThrottle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VDT.Core.Operators;
+
+public class QueueThrottle<TValue> : IOperator<TValue, TValue> {
+    private readonly Func<CancellationToken, Task<int>> delayFunc;
+    private readonly object nextPublishTimeLock = new();
+    private DateTime nextPublishTime = DateTime.MinValue;
+
+    internal Func<int, CancellationToken, Task> Delay { get; set; } = Task.Delay;
+    internal Func<DateTime> UtcNow { get; set; } = () => DateTime.UtcNow;
+
+    public QueueThrottle(int delayInMilliseconds)
+        : this(_ => Task.FromResult(delayInMilliseconds)) { }
+
+    public QueueThrottle(Func<int> delayFunc)
+        : this(_ => Task.FromResult(delayFunc())) { }
+
+    public QueueThrottle(Func<Task<int>> delayFunc)
+        : this(_ => delayFunc()) { }
+
+    public QueueThrottle(Func<CancellationToken, Task<int>> delayFunc) {
+        this.delayFunc = delayFunc;
+    }
+
+    public async Task Execute(TValue value, IOperandStream<TValue> targetStream, CancellationToken cancellationToken) {
+        var now = UtcNow();
+        var delayInMilliseconds = await delayFunc(cancellationToken);
+        double requiredDelayInMilliseconds;
+
+        lock (nextPublishTimeLock) {
+            requiredDelayInMilliseconds = (nextPublishTime - now).TotalMilliseconds;
+
+            if (requiredDelayInMilliseconds > 0) {
+                nextPublishTime = nextPublishTime.AddMilliseconds(delayInMilliseconds);
+            }
+            else {
+                nextPublishTime = now.AddMilliseconds(delayInMilliseconds);
+            }
+        }
+
+        if (requiredDelayInMilliseconds > 0) {
+            await Delay((int)requiredDelayInMilliseconds, cancellationToken);
+        }
+
+        await targetStream.Publish(value, cancellationToken);
+    }
+}
