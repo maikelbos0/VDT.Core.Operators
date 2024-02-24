5a99ac30e35662378bdd68d402fe88caaa48c1f4
Maikel Bos <maikel.bos0@gmail.com>
2024-02-24 08:05:09 +0100

    Add queue throttle operator stream extensions

---
+++ src/VDT.Core.Operators.Tests/OperandStreamQueueThrottleExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using NSubstitute;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VDT.Core.Operators.Tests;
+
+public class OperandStreamQueueThrottleExtensionsTests {
+    [Fact]
+    public void QueueThrottle_Constant() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.QueueThrottle(500);
+
+        subject.Received().Pipe(Arg.Any<QueueThrottle<string>>());
+    }
+
+    [Fact]
+    public void QueueThrottle_Function() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.QueueThrottle(() => 500);
+
+        subject.Received().Pipe(Arg.Any<QueueThrottle<string>>());
+    }
+
+    [Fact]
+    public void QueueThrottle_TaskFunction() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.QueueThrottle(() => Task.FromResult(500));
+
+        subject.Received().Pipe(Arg.Any<QueueThrottle<string>>());
+    }
+
+    [Fact]
+    public void QueueThrottle_CancellableTaskFunction() {
+        var subject = Substitute.For<IOperandStream<string>>();
+
+        _ = subject.QueueThrottle(_ => Task.FromResult(500));
+
+        subject.Received().Pipe(Arg.Any<QueueThrottle<string>>());
+    }
+}
---
+++ src/VDT.Core.Operators/OperandStreamQueueThrottleExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VDT.Core.Operators;
+
+public static class OperandStreamQueueThrottleExtensions {
+    public static IOperandStream<TValue> QueueThrottle<TValue>(this IOperandStream<TValue> operandStream, int delayInMilliseconds)
+        => operandStream.Pipe(new QueueThrottle<TValue>(delayInMilliseconds));
+
+    public static IOperandStream<TValue> QueueThrottle<TValue>(this IOperandStream<TValue> operandStream, Func<int> delayFunc)
+        => operandStream.Pipe(new QueueThrottle<TValue>(delayFunc));
+
+    public static IOperandStream<TValue> QueueThrottle<TValue>(this IOperandStream<TValue> operandStream, Func<Task<int>> delayFunc)
+        => operandStream.Pipe(new QueueThrottle<TValue>(delayFunc));
+
+    public static IOperandStream<TValue> QueueThrottle<TValue>(this IOperandStream<TValue> operandStream, Func<CancellationToken, Task<int>> delayFunc)
+        => operandStream.Pipe(new QueueThrottle<TValue>(delayFunc));
+}
