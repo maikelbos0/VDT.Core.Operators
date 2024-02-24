b1604df998b11db8f10bfda8169fc09bcf109ab3
Maikel Bos <maikel.bos0@gmail.com>
2024-02-24 08:10:42 +0100

    Add queue throttle docs

--- src/VDT.Core.Operators/OperandStreamQueueThrottleExtensions.cs
+++ src/VDT.Core.Operators/OperandStreamQueueThrottleExtensions.cs
@@ -4,16 +4,47 @@ using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
+/// <summary>
+/// Extension methods to more fluently apply operators to <see cref="IOperandStream{TValue}"/>
+/// </summary>
 public static class OperandStreamQueueThrottleExtensions {
+    /// <summary>
+    /// Pause at least the specified delay in between publishing values, queueing received values
+    /// </summary>
+    /// <typeparam name="TValue">Type of value to throttle</typeparam>
+    /// <param name="operandStream"><see cref="IOperandStream{TValue}"/> that will be subscribed to</param>
+    /// <param name="delayInMilliseconds">Time in milliseconds to wait in between publishing values</param>
+    /// <returns><see cref="IOperandStream{TValue}"/> that throttled values will be published to</returns>
     public static IOperandStream<TValue> QueueThrottle<TValue>(this IOperandStream<TValue> operandStream, int delayInMilliseconds)
         => operandStream.Pipe(new QueueThrottle<TValue>(delayInMilliseconds));
 
+    /// <summary>
+    /// Pause at least the specified delay in between publishing values, queueing received values
+    /// </summary>
+    /// <typeparam name="TValue">Type of value to throttle</typeparam>
+    /// <param name="operandStream"><see cref="IOperandStream{TValue}"/> that will be subscribed to</param>
+    /// <param name="delayFunc">Method that provides the time in milliseconds to wait in between publishing values</param>
+    /// <returns><see cref="IOperandStream{TValue}"/> that throttled values will be published to</returns>
     public static IOperandStream<TValue> QueueThrottle<TValue>(this IOperandStream<TValue> operandStream, Func<int> delayFunc)
         => operandStream.Pipe(new QueueThrottle<TValue>(delayFunc));
 
+    /// <summary>
+    /// Pause at least the specified delay in between publishing values, queueing received values
+    /// </summary>
+    /// <typeparam name="TValue">Type of value to throttle</typeparam>
+    /// <param name="operandStream"><see cref="IOperandStream{TValue}"/> that will be subscribed to</param>
+    /// <param name="delayFunc">Method that provides the time in milliseconds to wait in between publishing values</param>
+    /// <returns><see cref="IOperandStream{TValue}"/> that throttled values will be published to</returns>
     public static IOperandStream<TValue> QueueThrottle<TValue>(this IOperandStream<TValue> operandStream, Func<Task<int>> delayFunc)
         => operandStream.Pipe(new QueueThrottle<TValue>(delayFunc));
 
+    /// <summary>
+    /// Pause at least the specified delay in between publishing values, queueing received values
+    /// </summary>
+    /// <typeparam name="TValue">Type of value to throttle</typeparam>
+    /// <param name="operandStream"><see cref="IOperandStream{TValue}"/> that will be subscribed to</param>
+    /// <param name="delayFunc">Method that provides the time in milliseconds to wait in between publishing values</param>
+    /// <returns><see cref="IOperandStream{TValue}"/> that throttled values will be published to</returns>
     public static IOperandStream<TValue> QueueThrottle<TValue>(this IOperandStream<TValue> operandStream, Func<CancellationToken, Task<int>> delayFunc)
         => operandStream.Pipe(new QueueThrottle<TValue>(delayFunc));
 }
--- src/VDT.Core.Operators/QueueThrottle.cs
+++ src/VDT.Core.Operators/QueueThrottle.cs
@@ -4,7 +4,12 @@ using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
-public class QueueThrottle<TValue> : IOperator<TValue, TValue> {
+/// <summary>
+/// Operator that pauses at least the specified delay in between publishing values, queueing received values
+/// </summary>
+/// <typeparam name="TValue">Type of value to throttle</typeparam>
+public class QueueThrottle
+    <TValue> : IOperator<TValue, TValue> {
     private readonly Func<CancellationToken, Task<int>> delayFunc;
     private readonly object nextPublishTimeLock = new();
     private DateTime nextPublishTime = DateTime.MinValue;
@@ -12,19 +17,36 @@ public class QueueThrottle<TValue> : IOperator<TValue, TValue> {
     internal Func<int, CancellationToken, Task> Delay { get; set; } = Task.Delay;
     internal Func<DateTime> UtcNow { get; set; } = () => DateTime.UtcNow;
 
+    /// <summary>
+    /// Create a queued throttle operator
+    /// </summary>
+    /// <param name="delayInMilliseconds">Time in milliseconds to wait in between publishing values</param>
     public QueueThrottle(int delayInMilliseconds)
         : this(_ => Task.FromResult(delayInMilliseconds)) { }
 
+    /// <summary>
+    /// Create a queued throttle operator
+    /// </summary>
+    /// <param name="delayFunc">Method that provides the time in milliseconds to wait in between publishing values</param>
     public QueueThrottle(Func<int> delayFunc)
         : this(_ => Task.FromResult(delayFunc())) { }
 
+    /// <summary>
+    /// Create a queued throttle operator
+    /// </summary>
+    /// <param name="delayFunc">Method that provides the time in milliseconds to wait in between publishing values</param>
     public QueueThrottle(Func<Task<int>> delayFunc)
         : this(_ => delayFunc()) { }
 
+    /// <summary>
+    /// Create a queued throttle operator
+    /// </summary>
+    /// <param name="delayFunc">Method that provides the time in milliseconds to wait in between publishing values</param>
     public QueueThrottle(Func<CancellationToken, Task<int>> delayFunc) {
         this.delayFunc = delayFunc;
     }
 
+    /// <inheritdoc/>
     public async Task Execute(TValue value, IOperandStream<TValue> targetStream, CancellationToken cancellationToken) {
         var now = UtcNow();
         var delayInMilliseconds = await delayFunc(cancellationToken);
