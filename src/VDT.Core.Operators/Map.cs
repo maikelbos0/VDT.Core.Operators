486ae8368ec5954e24cf3ceab221119b93556e21
Maikel Bos <maikel.bos0@gmail.com>
2024-02-25 13:10:25 +0100

    Add documentation to map operator

--- src/VDT.Core.Operators/Map.cs
+++ src/VDT.Core.Operators/Map.cs
@@ -4,19 +4,37 @@ using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
+/// <summary>
+/// Operator that publishes values transformed from <typeparamref name="TValue"/> into <typeparamref name="TTransformedValue"/> by the supplied transformation method
+/// </summary>
+/// <typeparam name="TValue">Type of received values</typeparam>
+/// <typeparam name="TTransformedValue">Type of values to publish</typeparam>
 public class Map<TValue, TTransformedValue> : IOperator<TValue, TTransformedValue> {
     private readonly Func<TValue, CancellationToken, Task<TTransformedValue>> func;
 
+    /// <summary>
+    /// Create a map operator
+    /// </summary>
+    /// <param name="func">Method that transforms received values into values to publish</param>
     public Map(Func<TValue, TTransformedValue> func)
         : this((value, _) => Task.FromResult(func(value))) { }
 
+    /// <summary>
+    /// Create a map operator
+    /// </summary>
+    /// <param name="func">Method that transforms received values into values to publish</param>
     public Map(Func<TValue, Task<TTransformedValue>> func)
         : this((value, _) => func(value)) { }
 
+    /// <summary>
+    /// Create a map operator
+    /// </summary>
+    /// <param name="func">Method that transforms received values into values to publish</param>
     public Map(Func<TValue, CancellationToken, Task<TTransformedValue>> func) {
         this.func = func;
     }
 
+    /// <inheritdoc/>
     public async Task Execute(TValue value, IOperandStream<TTransformedValue> targetStream, CancellationToken cancellationToken)
         => await targetStream.Publish(await func(value, cancellationToken), cancellationToken);
 }
--- src/VDT.Core.Operators/OperandStreamMapExtensions.cs
+++ src/VDT.Core.Operators/OperandStreamMapExtensions.cs
@@ -4,13 +4,40 @@ using System.Threading.Tasks;
 
 namespace VDT.Core.Operators;
 
+/// <summary>
+/// Extension methods to more fluently apply operators to <see cref="IOperandStream{TValue}"/>
+/// </summary>
 public static class OperandStreamMapExtensions {
+    /// <summary>
+    /// Publish values transformed from <typeparamref name="TValue"/> into <typeparamref name="TTransformedValue"/> by the supplied transformation method
+    /// </summary>
+    /// <typeparam name="TValue">Type of received values</typeparam>
+    /// <typeparam name="TTransformedValue">Type of values to publish</typeparam>
+    /// <param name="operandStream"><see cref="IOperandStream{TValue}"/> that will be subscribed to</param>
+    /// <param name="func">Method that transforms received values into values to publish</param>
+    /// <returns><see cref="IOperandStream{TValue}"/> that transformed values will be published to</returns>
     public static IOperandStream<TTransformedValue> Map<TValue, TTransformedValue>(this IOperandStream<TValue> operandStream, Func<TValue, TTransformedValue> func)
         => operandStream.Pipe(new Map<TValue, TTransformedValue>(func));
 
+    /// <summary>
+    /// Publish values transformed from <typeparamref name="TValue"/> into <typeparamref name="TTransformedValue"/> by the supplied transformation method
+    /// </summary>
+    /// <typeparam name="TValue">Type of received values</typeparam>
+    /// <typeparam name="TTransformedValue">Type of values to publish</typeparam>
+    /// <param name="operandStream"><see cref="IOperandStream{TValue}"/> that will be subscribed to</param>
+    /// <param name="func">Method that transforms received values into values to publish</param>
+    /// <returns><see cref="IOperandStream{TValue}"/> that transformed values will be published to</returns>
     public static IOperandStream<TTransformedValue> Map<TValue, TTransformedValue>(this IOperandStream<TValue> operandStream, Func<TValue, Task<TTransformedValue>> func)
         => operandStream.Pipe(new Map<TValue, TTransformedValue>(func));
 
+    /// <summary>
+    /// Publish values transformed from <typeparamref name="TValue"/> into <typeparamref name="TTransformedValue"/> by the supplied transformation method
+    /// </summary>
+    /// <typeparam name="TValue">Type of received values</typeparam>
+    /// <typeparam name="TTransformedValue">Type of values to publish</typeparam>
+    /// <param name="operandStream"><see cref="IOperandStream{TValue}"/> that will be subscribed to</param>
+    /// <param name="func">Method that transforms received values into values to publish</param>
+    /// <returns><see cref="IOperandStream{TValue}"/> that transformed values will be published to</returns>
     public static IOperandStream<TTransformedValue> Map<TValue, TTransformedValue>(this IOperandStream<TValue> operandStream, Func<TValue, CancellationToken, Task<TTransformedValue>> func)
         => operandStream.Pipe(new Map<TValue, TTransformedValue>(func));
 }
