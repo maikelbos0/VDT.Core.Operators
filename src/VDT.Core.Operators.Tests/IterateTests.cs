8ea44b33abdb657fe04235448185bdf3b1c554ec
Maikel Bos <maikel.bos0@gmail.com>
2024-02-26 17:43:25 +0100

    Add iterate operator

---
+++ src/VDT.Core.Operators.Tests/IterateTests.cs
@@ -0,0 +1,23 @@
+﻿using NSubstitute;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VDT.Core.Operators.Tests;
+
+public class IterateTests {
+    [Fact]
+    public async Task Iterates() {
+        var subject = new Iterate<char>();
+        var targetStream = Substitute.For<IOperandStream<char>>();
+        var cancellationTokenSource = new CancellationTokenSource();
+
+        await subject.Execute("Foo", targetStream, cancellationTokenSource.Token);
+
+        Received.InOrder(() => {
+            targetStream.Publish('F', cancellationTokenSource.Token);
+            targetStream.Publish('o', cancellationTokenSource.Token);
+            targetStream.Publish('o', cancellationTokenSource.Token);
+        });
+    }
+}
