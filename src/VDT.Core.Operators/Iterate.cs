dc8f22f03c4ace6ddcd56391cfec6a8f9e2268fe
Maikel Bos <maikel.bos0@gmail.com>
2024-03-11 17:04:46 +0100

    Fix various small issues

--- src/VDT.Core.Operators/Iterate.cs
+++ src/VDT.Core.Operators/Iterate.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace VDT.Core.Operators;
 
 /// <summary>
-/// Operator that iterates over received values of <see cref="IEnumerable{T}"/> of <typeparamref name="TValue"/> and publishes each value
+/// Operator that iterates over received values of <see cref="IEnumerable{T}"/> of <typeparamref name="TValue"/> and publishes each item
 /// </summary>
 /// <typeparam name="TValue">Type of values</typeparam>
 public class Iterate<TValue> : IOperator<IEnumerable<TValue>, TValue> {
